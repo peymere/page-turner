8bb210d916e9f9f8c5a3e71a76fe6f308ab27c86
Peyton Meredith <peymeredith@gmail.com>
2023-12-12 12:03:59 -0600

    started book club model

--- server/models.py
+++ server/models.py
@@ -34,3 +34,27 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password_string):
         byte_object = password_string.encode('utf-8')
         return bcrypt.check_password_hash(self._password_hash, byte_object)
+    
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}>"
+    
+class BookClub(db.Model, SerializerMixin):
+    __tablename__ = 'book_clubs'
+
+    # serialize_rules=('-_password_hash',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String(300))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # owner = db.relationship('User', backref='book_clubs_owned')
+    owner = db.relationship('User', backref='book_clubs_owned')
+    members = db.relationship('User', secondary='book_clubs_users', backref='book_clubs_joined')
+    books = db.relationship('Book', secondary='book_clubs_books', backref='book_clubs')
+    meetings = db.relationship('Meeting', backref='book_club')
+
+    def __repr__(self):
+        return f"<BookClub #{self.id}: {self.name}>"
