1439c126638c26a12b98c26167ee42e923f10685
Peyton Meredith <peymeredith@gmail.com>
2023-12-18 13:23:13 -0600

    finish member cards

--- client/src/components/ClubMembers.jsx
+++ client/src/components/ClubMembers.jsx
@@ -2,6 +2,7 @@ import { NavLink, useParams } from 'react-router-dom';
 import { LiaCrownSolid } from "react-icons/lia";
 import { useContext } from 'react';
 import Card from 'react-bootstrap/Card';
+import { Row, Col } from 'react-bootstrap';
 
 // local imports
 import styles from '../stylesheets/ClubProfile.module.css';
@@ -11,10 +12,13 @@ function ClubMembers() {
     const club = useContext(ClubContext);
 
     return (
+        <div className={styles.members_component}>
+        <h3 className={styles.members_header} >Members</h3>
         <div className={styles.members_list}>
-            <h3 className={styles.members_header} >Members</h3>
+            
             <div className={styles.member_card_container}>
-                <Card style={{ width: '14rem' }}>
+                
+                <Card style={{ width: '14rem' }} className={styles.member_card}>
                     <Card.Img 
                         variant="top" 
                         src={club?.owner.profile_pic ? club?.owner.profile_pic : "/src_images/placeholder-prof-pic.png"} 
@@ -23,15 +27,18 @@ function ClubMembers() {
                     />
                     <Card.Body className={styles.member_card_body}>
                         <Card.Title>
-                            <LiaCrownSolid /> {' '}
-                            <NavLink to={`/userprofile/${club?.owner.id}`} >
+                            
+                            <NavLink className={styles.member_link} to={`/userprofile/${club?.owner.id}`} >
+                                <LiaCrownSolid  /> {' '}
                                 @{club?.owner.username}
                             </NavLink>
                         </Card.Title>
                     </Card.Body>
                 </Card>
+                
                 {club?.members.map((member, idx) => (
-                    <Card style={{ width: '14rem' }}>
+                    
+                    <Card  className={styles.member_card}>
                         <Card.Img 
                         variant="top" 
                         src={member.profile_pic ? member.profile_pic : "/src_images/placeholder-prof-pic.png"} 
@@ -39,15 +46,18 @@ function ClubMembers() {
                         className={styles.member_img}  
                         />
                         <Card.Body className={styles.member_card_body} >
-                            <Card.Title>
-                            <NavLink to={`/userprofile/${member.id}`}>
+                            <Card.Title className={styles.member_usernames}>
+                            <NavLink to={`/userprofile/${member.id}`} className={styles.member_link}>
                                 @{member.username}
                             </NavLink>
                             </Card.Title>
                         </Card.Body>
                     </Card>
+                    
                 ))}
+                
             </div>
+        </div>
         </div>
     );
 }
