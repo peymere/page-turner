cd5f6f2be79a64fbd30a8def772f50928a762c7a
Peyton Meredith <peymeredith@gmail.com>
2023-12-18 19:36:25 -0600

    frontend book api fetch, making book cards

--- client/src/components/Books.jsx
+++ client/src/components/Books.jsx
@@ -1,9 +1,46 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Card from 'react-bootstrap/Card'
+import { NavLink } from 'react-router-dom'
 
+// local imports
+import styles from '../stylesheets/Books.module.css'
 
 function Books() {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        fetch('https://openlibrary.org/search.json?q=first_publish_year%3A[2020+TO+2023]&language%3Aeng&subject=fiction&limit=20&page=2&fields=title+author_name+cover_i')
+        .then((resp) => {
+            if (resp.ok) {
+                return resp.json()
+            } else {
+                console.log('error')
+            }
+            })
+        .then((bookData) => {
+            setBooks(bookData.docs)
+        })
+        .catch((err) => {
+            console.log("Error getting books:", err);
+        })
+    }, [])
+
+    // console.log('Books:', books)
+
     return (
-        <div>
-            <h1>Books</h1>
+        <div className={styles.book_component}>
+        {books.map((book, idx) => (
+            
+                <div key={idx}>
+                    <h1>{book.title}</h1>
+                    <h2>{book.author_name[0]}</h2>
+                    <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} alt="book cover" />
+                </div>
+        )
+
+        
+        )}
         </div>
     )
 }
